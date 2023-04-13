f61e12b6ff04b31606583d1ffd64d6abf2e84116
Josue Marquez <owaruuu@gmail.com>
2023-04-12 22:29:07 -0400

    remove hover rules
    -remove hover classes on mobile

--- index.js
+++ index.js
@@ -4,6 +4,35 @@
 
 import {ReplaceState, PushHistoryState} from "./history.js";
 
+//Seccion Touch Watch
+
+function watchForHover() {
+    // lastTouchTime is used for ignoring emulated mousemove events
+    let lastTouchTime = 0;
+  
+    function enableHover() {
+      if (new Date() - lastTouchTime < 500) return
+      document.body.classList.add('hasHover');
+    }
+  
+    function disableHover() {
+      document.body.classList.remove('hasHover');
+    }
+  
+    function updateLastTouchTime() {
+      lastTouchTime = new Date();
+    }
+  
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+  
+    enableHover();
+  }
+  
+  watchForHover();
+//Fin Seccion Touch Watch
+
 //Seccion History
 let state = {
     currentPage : "home",
